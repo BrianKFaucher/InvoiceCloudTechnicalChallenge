38264355a1eb0bc9fd8e7030d5a6aab660ec14c1
Brian Faucher <briankfaucher@msn.com>
2023-11-03 09:35:38 -0700

    Switching test to Gherkin syntax

--- SeleniumTestProject/HerokuappTests.cs
+++ SeleniumTestProject/HerokuappTests.cs
@@ -12,27 +12,42 @@ namespace SeleniumTestProject
         public void VerifyAddElementButtonAddsDesiredElementsToPage()
         {
             var pageUrl = "https://the-internet.herokuapp.com/add_remove_elements/";
+            var pageTitle = "The Internet";
             var countDeleteButtons = 10;
+            using var driver = CreateChromeDriver(Directory.GetCurrentDirectory());
 
-            using var driver = CreateChromeDriver(Directory.GetCurrentDirectory(), true);
+            GivenAddRemoveElementsPage(driver, pageUrl, pageTitle);
+
+            WhenAddElementButtonIsClickedNtimes(driver, countDeleteButtons);
+
+            ThenPageContainsCorrespondingNumberOfElements(driver, countDeleteButtons);
+        }
+
+        private static void GivenAddRemoveElementsPage(IWebDriver driver, string pageUrl, string pageTitle)
+        {
             driver.Navigate().GoToUrl(pageUrl);
-            driver.Title.Should().Be("The Internet");
             driver.Url.Should().Be(pageUrl);
+            driver.Title.Should().Be(pageTitle);
+        }
 
-            for(int i = 0; i < countDeleteButtons; i++)
+        private static void WhenAddElementButtonIsClickedNtimes(IWebDriver driver, int countDeleteButtons)
+        {
+            for (int i = 0; i < countDeleteButtons; i++)
             {
                 driver.FindElement(By.XPath(@"//button[text()=""Add Element""]")).Click();
             }
+        }
 
+        private static void ThenPageContainsCorrespondingNumberOfElements(IWebDriver driver, int countDeleteButtons)
+        {
             var allElements = driver.FindElements(By.XPath(@"//button[text()=""Delete""]"));
             allElements.Count.Should().Be(countDeleteButtons);
         }
 
-        private static IWebDriver CreateChromeDriver(string driverDirectory, bool isDebuggerAttached)
+        private static IWebDriver CreateChromeDriver(string driverDirectory)
         {
             var options = new ChromeOptions();
             options.AddArgument("--headless");
-
             return new ChromeDriver(driverDirectory, options);
         }
     }
